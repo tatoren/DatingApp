b7f8033f3221474e71d5cc386f570e22335e3b50
tatoren <one_with_all@hotmail.com>
2019-11-25 15:15:06 -0700

    Error Handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in Successfuly');
     }, error => {
-      console.log('Failed to log in.');
+      console.log(error);
   });
   }
 
