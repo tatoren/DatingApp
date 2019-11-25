029d25383347e74a434484a26308afe54dad40b5
tatoren <one_with_all@hotmail.com>
2019-11-25 12:11:48 -0700

    Login and Register Function

--- DatingApp.api/Controllers/ValuesController.cs
+++ DatingApp.api/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace DatingApp.api.Controllers
                 _context = context;
             }
         // GET api/values
-        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
